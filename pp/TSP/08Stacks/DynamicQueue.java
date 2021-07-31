ac1a49a873cdb3fc8038d8a3709175d769c68542
Jyoti Kashyap <jyotikas1608@gmail.com>
2021-07-31 21:02:20 +0530

    dynamic queue

---
+++ pp/TSP/08Stacks/DynamicQueue.java
@@ -0,0 +1,76 @@
+package com.Test;
+
+public class DynamicQueue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static class CustomQueue {
+	    int[] data;
+	    int front;
+	    int size;
+
+	    public CustomQueue(int cap) {
+	      data = new int[cap];
+	      front = 0;
+	      size = 0;
+	    }
+
+	    int size() {
+	    	return size;
+	    }
+
+	    void display() {
+	    	 for(int i=0;i<this.size;i++) {
+		    	  int indx = (i + front) % data.length;
+		    	  System.out.print(data[indx]+" ");
+		      }
+		      System.out.println();
+	    }
+
+	    void add(int val) {
+	      if(size() == data.length) {
+	    	  int [] ndata = new int[2*data.length];
+	    	  for(int i=0;i<data.length;i++) {
+	    		  int indx = (front+i)%data.length;
+	    		  ndata[i] = data[indx];
+	    	  }
+	    	  front = 0;
+	    	  int indx = (front + size)%data.length;
+	    	  data[indx] = val;
+	    	  size++;
+	    	  
+	      }else {
+	    	  int indx = (front + size)%data.length;
+	    	  data[indx] = val;
+	    	  size++;
+	      }
+	    }
+
+	    int remove() {
+	      // write ur code here
+	    	if(size() == 0) {
+		    	  System.out.print("Queue underflow");
+		    	  return -1;
+		      }else {
+		    	  int val = data[front];
+		    	  front = (front + 1)%data.length;
+		    	  size--;
+		    	  return val;
+		      }
+	    }
+
+	    int peek() {
+	    	 if(size() == 0) {
+		    	  System.out.print("Queue underflow");
+		    	  return -1;
+		      }else {
+		    	  int val = data[front];		    	  
+		    	  return val;
+		      }
+	    }
+	  }
+
+}
