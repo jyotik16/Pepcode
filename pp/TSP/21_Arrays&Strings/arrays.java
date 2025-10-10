0a3ad2a1b32191bb5333dd2111084384cce519d0
Jyoti Kashyap <jyotikas1608@gmail.com>
2025-10-10 18:12:48 +0530

    MinClosetDistance Code leetcode 849

--- pp/TSP/21_Arrays&Strings/arrays.java
+++ pp/TSP/21_Arrays&Strings/arrays.java
@@ -451,6 +451,54 @@ public class arrays {
         return Math.max(zeros, dist);
     }
 
+    int maxDistToClosest2jn(int[] seats) {
+        int n = seats.length;
+
+        int[] left = new int[n];
+        Arrays.fill(left, -1);
+
+        int[] right = new int[n];
+        Arrays.fill(right, -1);
+
+        int i = 0;
+
+        int lastOne  = -1;
+        int maxDis = -1;
+
+        while(i<n){
+            if(seats[i] == 1){
+                lastOne = i;
+            } else {
+                left[i] = lastOne;
+                maxDis = 1;
+            }
+            i++;
+        }
+
+        i = n-1;
+        lastOne  = -1;
+        while(i>=0){
+            if(seats[i] == 1){
+                lastOne = i;
+            } else {
+                right[i] = lastOne;
+            }
+            i--;
+        }
+
+        for(i=0;i<n;i++){
+            if(seats[i] == 0){
+                int leftDis = (left[i] !=-1)? i -left[i] : Integer.MAX_VALUE;
+                int rightDis = (right[i] !=-1)? right[i] - i : Integer.MAX_VALUE;
+                int currDis = Math.min(leftDis, rightDis);
+                if(currDis >maxDis){
+                    maxDis = currDis;
+                }
+            }
+        }
+        return maxDis;
+    }
+
     // leetcode 41. https://leetcode.com/problems/first-missing-positive/
     public int firstMissingPositive(int[] nums) {
         int n = nums.length;
