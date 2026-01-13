0b4abaaf839732c441336344faa456c9d2bb6a81
Jyoti Kashyap <jyotikas1608@gmail.com>
2026-01-13 15:15:09 +0530

    closet elements approach 2 && 3

--- pp/TSP/23_Searching&Sorting/SnS.java
+++ pp/TSP/23_Searching&Sorting/SnS.java
@@ -215,6 +215,50 @@ public class SnS {
         return res;
     }
 
+        public static List<Integer> findClosestElements2(int[] arr, int k, int x) {
+
+        // The sliding window is between 'mid' and 'mid' + k.
+        int left = 0, right = arr.length - k;
+        while (left < right) {
+            int midpoint = left + (right - left) / 2; // same as (left + right) / 2
+            // With midpoint on the left, we use x - arr[midpoint], while arr[midpoint + k] - x because it is on the right.
+            // This is important!
+            // Rather than using Math.abs(), we need the direction keep the x within the sliding window.
+            // If the window is too far left, we shift the window to the right.
+            if (x - arr[midpoint] > arr[midpoint + k] - x) {
+                left = midpoint + 1;
+            }
+            // If the window is too far right, we shift the window to the left.
+            else {
+                right = midpoint;
+            }
+        }
+        List<Integer> result = new ArrayList<>(k);
+        for (int i = left; i < left + k; i++) {
+            result.add(arr[i]);
+        }
+        return result;
+    }
+
+    public static List<Integer> findClosestElements3(int[] arr, int k, int x) {
+        int start = 0;
+        int end = arr.length - 1;
+        // Between the 'start' and 'end' pointers, inclusive, contains all the k integers that is closest to x.
+        while (end - start >= k) {
+            // Move 'start' to the right if 'end' is closer to x, or move 'end' to the left if 'start' is closer to x.
+            if (Math.abs(arr[start] - x) > Math.abs(arr[end] - x)) {
+                start++;
+            } else {
+                end--;
+            }
+        }
+        List<Integer> result = new ArrayList<>(k);
+        for (int i = start; i <= end; i++) {
+            result.add(arr[i]);
+        }
+        return result;
+    }
+
     // find pair with given difference, portal
     public static void findPair(int[]arr,int target) {
         int lo = 0;
